3480e81ec171ad4d0da69afa09abf1613db8701e
= <=>
2025-04-21 01:19:35 +0500

    opened cors

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 import cookieParser from 'cookie-parser'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('timer-api')
-  app.use(cookieParser())
-  app.enableCors({
-    origin: ['https://bb-timer.vercel.app/'],
-    credentials: true,
-    exposedHeaders: 'set-cookie'
-  })
-  await app.listen(5000);
+	const app = await NestFactory.create(AppModule)
+	app.setGlobalPrefix('timer-api')
+	app.enableCors({
+		origin: [
+			'http://127.0.0.1:3000/',
+			'http://127.0.0.1:3000',
+			'http://localhost:3000/',
+			'http://localhost:3000',
+			'http://192.168.31.223:3000/',
+			'http://192.168.31.223:3000'
+		],
+		methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+		credentials: true
+	})
+	app.use(cookieParser('localhost'))
+	await app.listen(5000)
 }
-bootstrap();
+bootstrap()
